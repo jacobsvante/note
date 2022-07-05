a809452f66891c371bddad95b3caaa7527ad8eb7
Jacob Magnusson <m@jacobian.se>
2022-07-05 14:13:41 +0200

    feat: Update to stable clap

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,18 @@
 use std::path::PathBuf;
 
 use chrono::{DateTime, Local, NaiveDate, ParseError};
-use clap::{AppSettings, Clap};
 
-fn parse_notes_dir(src: &str) -> Result<PathBuf, Box<dyn std::error::Error>> {
+fn parse_notes_dir(src: &str) -> Result<PathBuf, String> {
     let src = shellexpand::tilde(src).to_string();
     let p = PathBuf::from(src);
-    p.metadata()?;
-    Ok(p)
+    match p.metadata() {
+        Ok(_) => Ok(p),
+        Err(error) if error.kind() == std::io::ErrorKind::NotFound => {
+            eprintln!("Notes directory {p:?} does not exist");
+            Ok(p)
+        }
+        Err(error) => Err(error.to_string()),
+    }
 }
 
 fn parse_date(src: &str) -> Result<DateTime<Local>, ParseError> {
@@ -22,8 +27,8 @@ fn parse_date(src: &str) -> Result<DateTime<Local>, ParseError> {
     }
 }
 
-#[derive(Clap)]
-#[clap(version = "0.1", setting = AppSettings::ColoredHelp)]
+#[derive(clap::Parser)]
+#[clap(version = "0.1")]
 pub struct Cli {
     #[clap(short, long, default_value = "info")]
     pub log_level: String,
@@ -37,33 +42,18 @@ pub struct Cli {
     pub subcmd: SubCommand,
 }
 
-#[derive(Clap)]
+#[derive(clap::Subcommand)]
 pub enum SubCommand {
-    New(HandleNew),
-    Edit(HandleEdit),
-    List(HandleList),
+    /// Create a new note and edit it in your default editor
+    ///
+    /// The env var VISUAL or EDITOR is used to detect the correct editor.
+    /// Date and base directory is specified by the parent command
+    New { name: String },
+    /// Edit an existing note it in your default editor
+    ///
+    /// The env var VISUAL or EDITOR is used to detect the correct editor.
+    /// Date and base directory is specified by the parent command/
+    Edit { name: String },
+    /// List all notes that exist
+    List,
 }
-
-/// Create a new note and edit it in your default editor
-///
-/// The env var VISUAL or EDITOR is used to detect the correct editor.
-/// Date and base directory is specified by the parent command
-#[derive(Clap)]
-pub struct HandleNew {
-    #[clap()]
-    pub name: String,
-}
-
-/// Edit an existing note it in your default editor
-///
-/// The env var VISUAL or EDITOR is used to detect the correct editor.
-/// Date and base directory is specified by the parent command/
-#[derive(Clap)]
-pub struct HandleEdit {
-    #[clap()]
-    pub name: String,
-}
-
-/// List all notes that exist for the given date
-#[derive(Clap)]
-pub struct HandleList {}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
+use clap::Parser;
+use note::{cli, logging};
 use std::{io::Write, os::unix::prelude::OsStrExt};
 
-use note::{cli, logging};
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let opts = <cli::Cli as clap::Clap>::parse();
+    let opts = cli::Cli::parse();
     logging::init(&opts.log_level);
 
     match opts.subcmd {
-        cli::SubCommand::New(subopts) => {
-            let file_path =
-                note::make_file_path(opts.base_directory, opts.date.date(), &subopts.name)?;
-            note::new(file_path.clone(), &subopts.name)?;
+        cli::SubCommand::New { name } => {
+            let file_path = note::make_file_path(opts.base_directory, opts.date.date(), &name)?;
+            note::new(file_path.clone(), &name)?;
             note::edit(file_path)?;
         }
-        cli::SubCommand::Edit(subopts) => {
-            let file_path =
-                note::make_file_path(opts.base_directory, opts.date.date(), &subopts.name)?;
+        cli::SubCommand::Edit { name } => {
+            let file_path = note::make_file_path(opts.base_directory, opts.date.date(), &name)?;
             note::edit(file_path)?;
         }
-        cli::SubCommand::List(_subopts) => {
+        cli::SubCommand::List => {
             let mut stdout = std::io::stdout();
             for n in note::list(opts.base_directory, opts.date.date())? {
                 stdout.write_all(n.as_bytes())?;
